5d9e3e39e1b53b81ecd116011fe7eed43d695988
Tahjudinsherasiya <138747492+Tahjudinsherasiya@users.noreply.github.com>
2023-09-20 16:50:48 +0530

    Add files via upload

--- Assessment1.py.py
+++ Assessment1.py.py
@@ -1,35 +1,37 @@
-notes = {}
-
+notes = []
 def generate_note():
-    title = input("Enter the title of the note: ")
-    content = input("Enter the content of the note: ")
-    notes[title] = content
-    print(f"Note '{title}' has been created.")
+    note = input("Enter your note: ")
+    notes.append(note)
+    print("Note generat successfully!")
 
-def view_note():
-    title = input("Enter the title of the note you want to view: ")
-    if title in notes:
-        print(f"Title: {title}")
-        print(f"Content: {notes[title]}")
+def view_notes():
+    if notes:
+        print("\nYour Notes:")
+        for idx, note in enumerate(notes, 1):
+            print(f"{idx}. {note}")
     else:
-        print(f"Note '{title}' not found.")
+        print("No notes.")
 
 while True:
     print("\nMenu:")
     print("1. Generate Note")
     print("2. View Note")
-    print("4. Quit")
+    print("4. Exit")
 
-    choice = input("Enter your choice (1/2/4): ")
-    
-    if choice == '1':
-        generate_note()
-    elif choice == '2':
-        view_note()
-    elif choice == '4':
-        print("Exiting the application. Goodbye!")
-        break
+    choice = input("Enter your choice: (1/2/4): ")
+
+    if choice.isdigit():
+        choice = int(choice)
+        if choice == 1:
+            generate_note()
+        elif choice == 2:
+            view_notes()
+        elif choice == 4:
+            print("Exit...")
+            break
+        else:
+            print("Invalid choice. Please enter 1, 2, or 4.")
     else:
-        print("Invalid choice Please choose 1, 2, or 4.")
-
-
+        print("Invalid input. Please enter (1, 2, or 4).")
+        
+        
